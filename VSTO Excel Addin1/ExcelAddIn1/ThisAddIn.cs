b6e7e43867216e223ea65c0e43216acff191b419
Shawn McDowell <shawnmc@microsoft.com>
2018-02-02 12:49:47 -0800

    Adding VSTO Excel Addin1

---
+++ VSTO Excel Addin1/ExcelAddIn1/ThisAddIn.cs
@@ -0,0 +1,72 @@
+﻿using ExcelAddIn1;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using Microsoft.Office.Tools;
+
+namespace DDPIExcelAddIn
+{
+    public partial class ThisAddIn
+    {
+        private IntPtr m_hProcess;
+        private CustomTaskPane myCustomTaskPane;
+
+        public Excel.Application ExcelApp;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            ExcelApp = Globals.ThisAddIn.Application;
+
+            ExcelApp.WorkbookBeforeSave += new Microsoft.Office.Interop.Excel.AppEvents_WorkbookBeforeSaveEventHandler(Application_WorkbookBeforeSave);
+            m_hProcess = Process.GetCurrentProcess().Handle;
+            DPIHelper.DebugPrintDPIAwareness(m_hProcess, "startup");
+
+            // Add a custom taskpane - Win Form
+            UserControlWinForm myUserControl1 = new UserControlWinForm();
+            DPIHelper.DebugPrintDPIAwareness(m_hProcess, "after usercontrol1 create");
+            myCustomTaskPane = this.CustomTaskPanes.Add(myUserControl1, "WinForm");
+            myCustomTaskPane.Width = 500;
+            myCustomTaskPane.Visible = true;
+
+            DPIHelper.DebugPrintDPIAwareness(m_hProcess, "startup end");
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            DPIHelper.DebugPrintDPIAwareness(m_hProcess, "** shutdown");
+        }
+
+        void Application_WorkbookBeforeSave(Microsoft.Office.Interop.Excel.Workbook Wb, bool SaveAsUI, ref bool Cancel)
+        {
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)ExcelApp.ActiveSheet);
+            Excel.Range firstRow = activeWorksheet.get_Range("A1");
+            firstRow.EntireRow.Insert(Excel.XlInsertShiftDirection.xlShiftDown);
+            Excel.Range newFirstRow = activeWorksheet.get_Range("A1");
+            newFirstRow.Value2 = String.Format("Save triggered at {0}, {1}", DateTime.Now, DPIHelper.DPIAwarenessText("BeforeSave"));
+            DPIHelper.DebugPrintDPIAwareness(m_hProcess, "BeforeSave");
+
+            System.Windows.Forms.Form f1 = new Form1();
+            f1.ShowDialog();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
---
+++ VSTO Excel Addin1/ExcelAddIn1/WinForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace DDPIExcelAddIn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlWinForm1 = new DDPIExcelAddIn.UserControlWinForm();
+            this.SuspendLayout();
+            // 
+            // userControlWinForm1
+            // 
+            this.userControlWinForm1.Location = new System.Drawing.Point(12, 12);
+            this.userControlWinForm1.Name = "userControlWinForm1";
+            this.userControlWinForm1.Size = new System.Drawing.Size(307, 532);
+            this.userControlWinForm1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 635);
+            this.Controls.Add(this.userControlWinForm1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControlWinForm userControlWinForm1;
+    }
+}
