72fb5f9ec184fcf8778f378b9a3d0482a52f842e
Shawn McDowell <shawnmc@microsoft.com>
2018-02-01 16:36:05 -0800

    Adding VSTO WordAddin1

---
+++ VSTO WordAddIn1/WordAddIn1/ThisAddIn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools;
+using Microsoft.Office.Tools.Word;
+
+namespace WordAddIn1
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            UserControl1 user1 = new UserControl1();
+            CustomTaskPane myCustomTaskPane = this.CustomTaskPanes.Add(user1, "WinForm Add-in");
+            myCustomTaskPane.Visible = true;
+
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
